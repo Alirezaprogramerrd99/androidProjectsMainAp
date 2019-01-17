9cd04247a942fd0cbc7e5da7ef8a20376f5315a8
Alireza <arashidi1378@gmail.com>
2019-01-17 11:18:04 +0330

    Alirezas Part2

--- app/src/main/java/com/example/alireza/myapplicationfirst/MyAdpter.java
+++ app/src/main/java/com/example/alireza/myapplicationfirst/MyAdpter.java
@@ -1,7 +1,9 @@
 package com.example.alireza.myapplicationfirst;
 
 import android.support.annotation.NonNull;
+import android.support.v7.widget.AppCompatTextView;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -26,11 +28,15 @@ public class MyAdpter extends RecyclerView.Adapter <MyAdpter.MyViewHolder> {
     @NonNull
     @Override
     public MyViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int viewType) {
-        return null;
+       View itemView = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.my_tasks_list1, viewGroup, false);
+
+        return new MyViewHolder(itemView);
     }
 
     @Override
     public void onBindViewHolder(@NonNull MyViewHolder myViewHolder, int position) {
+
+        myViewHolder.bind(tasks.get(position));  // index ha ro set mekhoonee. to view holder
 
     }
 
@@ -41,8 +47,22 @@ public class MyAdpter extends RecyclerView.Adapter <MyAdpter.MyViewHolder> {
 
     static class MyViewHolder extends RecyclerView.ViewHolder{
 
+        AppCompatTextView massageTextView;
+        AppCompatTextView DateTextView;  // inja bayad baraye eleman ha digar field dar nazar
+
+        // gerfete shavad.
         MyViewHolder(View itemView){
             super(itemView);
+            massageTextView = itemView.findViewById(R.id.masssageTxt); // ref ing massage txt to xml code.
+            DateTextView = itemView.findViewById(R.id.DateTxt);
+        }
+
+        public void bind(Task task){  // field ha ro por mikoneee. toye on bind view holder
+
+            massageTextView.setText(task.getDate());
+            DateTextView.setText(task.getDate());
+
+
         }
 
     }
